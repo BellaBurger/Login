f1b1c8ca2476b6f9812c0997281b18dc91a160e9
BellaBurger <43035550+BellaBurger@users.noreply.github.com>
2020-02-08 17:45:20 +0100

    Update login.js

--- login.js
+++ login.js
@@ -80,15 +80,3 @@ app.get('/home', function(request, response) {
 
 //Listen in port 3000
 app.listen(3000);
-
-
-
-/*
-var http = require('http');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
-
-*/
